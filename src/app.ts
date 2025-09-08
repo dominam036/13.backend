ea95fe8a62b171f99f72295db32e0d0a3d878ba9
dominam <dominik.namenyi@gmail.com>
2025-09-08 14:34:16 +0200

    Kutya table

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,27 @@
-console.log("haliho!")
-console.log("haliasdfasdfho2!")
-console.log("asdfgasf")
+import { data } from "./data";
+
+const table = document.createElement("table");
+document.body.appendChild(table);
+const thead = document.createElement("thead");
+table.appendChild(thead);
+const tbody = document.createElement("tbody");
+table.appendChild(tbody);
+
+const trHead = document.createElement("tr");
+thead.appendChild(trHead);
+
+Object.keys(data[0]).forEach(key => {
+    const th = document.createElement("th");
+    th.innerText = key;
+    trHead.appendChild(th);
+});
+
+data.forEach(kutya => {
+    const tr = document.createElement("tr");
+    tbody.appendChild(tr);
+    Object.keys(kutya).forEach(value => {
+        const td = document.createElement("td");
+        td.innerText = kutya[value].toString();
+        tr.appendChild(td);
+    })
+})
---
+++ src/kutya.ts
@@ -0,0 +1,8 @@
+export class Kutya {
+    "id":number;
+    "nev":string;
+    "fajta":string;
+    "nem":boolean;
+    "eletkor":number
+    "kepUrl":string;
+}
